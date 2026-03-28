58d42bae0e8882b200525155fcba160bc0e6935f
rimple137 <rimple956@gmail.com>
2026-03-29 00:13:27 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# student-ml-project
-ML project to predict student performance
+# 🎓 Student Performance Prediction
+
+## 📌 Project Overview
+
+This project predicts student marks based on study hours using Machine Learning.
+
+## 🚀 Features
+
+* Data analysis
+* Visualization
+* Prediction using Linear Regression
+
+## ▶️ How to Run
+
+pip install -r requirements.txt
+python main.py
---
+++ main.py.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+data = pd.read_csv("students.csv")
+
+print(data.head())
+print(data.info())  # columns, data types, missing values
+print(data.describe())  #stats (mean, min, max etc.)
+data.plot(x='study_hours', y='overall_score', kind='scatter')
+plt.show()
+X = data[['study_hours']]   # input
+y = data['overall_score']     # output
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+model = LinearRegression()
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+print(predictions)
+
+results = pd.DataFrame({
+    "Actual": y_test,
+    "Predicted": predictions
+})
+
+print(results)
+plt.figure()
+
+plt.scatter(X_test, y_test, color='blue', label='Actual')
+plt.scatter(X_test, predictions, color='red', label='Predicted')
+
+plt.legend()
+plt.xlabel("Input")
+plt.ylabel("Output")
+
+plt.show()
+# Take user input
+hours = float(input("Enter study hours: "))
+
+# Convert into proper format
+new_data = [[hours]]
+
+# Predict
+prediction = model.predict(new_data)
+
+# Show result
+print("Predicted Marks:", prediction[0])
